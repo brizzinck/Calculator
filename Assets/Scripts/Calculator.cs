f83fb9834cedebeff9a52a562c422a3bdc5d5b8a
Skalse <87083857+brizzinck@users.noreply.github.com>
2022-06-24 17:07:53 +0300

    Calculator

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Calculator : MonoBehaviour
+{
+    private ControllOperator controllOperators;
+    private double result = 0;
+    private string output = string.Empty;
+    private Stack<char> operStack = new Stack<char>();
+    private Stack<double> temp = new Stack<double>();
+    private string currentNumber = string.Empty;
+    public void Сalculate(TextMeshProUGUI textInformation)
+    {
+        textInformation.text = Calculating(controllOperators.EditingOperators(textInformation.text));
+    }
+    private void Start()
+    {
+        controllOperators = new ControllOperator();
+    }
+    private string Calculating(string input) => Counting(GetExpression(input));
+    private string GetExpression(string input)
+    {
+        output = string.Empty;
+        operStack = new Stack<char>();
+
+        try
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (IsDelimeter(input[i]))
+                    continue;
+                if (Char.IsDigit(input[i]) || input[i] == '-')
+                    i = DistributeNumbers(input, i);
+                if (controllOperators.IsChar(input[i]))
+                    DistributeOperators(input[i]);
+            }
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+            return output;
+        }
+        catch
+        {
+            return "Вираз невірний!!!";
+        }
+    }
+    private int DistributeNumbers(string input, int i)
+    {
+        while (!IsDelimeter(input[i]) && !controllOperators.IsChar(input[i]))
+        {
+            output += input[i];
+            i++;
+            if (i == input.Length) break;
+        }
+        i--;
+        return i;
+    }
+    private void DistributeOperators(char c)
+    {
+        if (c == '(')
+            operStack.Push(c);
+        else
+                    if (c == ')')
+        {
+            char s = operStack.Pop();
+
+            while (s != '(')
+            {
+                output += s.ToString() + ' ';
+                s = operStack.Pop();
+            }
+        }
+        else
+        {
+            if (operStack.Count > 0)
+                if (GetPriority(c) <= GetPriority(operStack.Peek()))
+                    output += operStack.Pop().ToString();
+            operStack.Push(char.Parse(c.ToString()));
+        }
+        output += " ";
+    }
+    private string Counting(string input)
+    {
+        result = 0;
+        temp = new Stack<double>();
+        try
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]) || input[i] == '-')
+                    i = WriteNumbers(input, i);
+                else if (controllOperators.IsChar(input[i]))
+                    CountingTwoNumbers(input[i]);    
+            }
+            return temp.Peek().ToString();
+        }
+        catch
+        {
+            return "Вираз невірний!!!";
+        }
+    }
+    private int WriteNumbers(string input, int i)
+    {
+        currentNumber = string.Empty;
+
+        while (!IsDelimeter(input[i]) && !controllOperators.IsChar(input[i]))
+        {
+            currentNumber += input[i];
+            i++;
+            if (i == input.Length) break;
+        }
+        temp.Push(double.Parse(currentNumber));
+        i--;
+        return i;
+    }
+    private void CountingTwoNumbers(char c)
+    {
+        double a = temp.Pop();
+        double b = temp.Pop();
+
+        switch (c)
+        {
+            case '+': result = b + a; break;
+            case '*': result = b * a; break;
+            case '/': result = b / a; break;
+            case '%': result = b * (a / 100); break;
+            case '^': result = double.Parse(Math.Pow(double.Parse(b.ToString()), double.Parse(a.ToString())).ToString()); break;
+        }
+        temp.Push(result);
+    }
+    private bool IsDelimeter(char c)
+    {
+        if ((" ".IndexOf(c) != -1))
+            return true;
+        return false;
+    }
+    private byte GetPriority(char c)
+    {
+        switch (c)
+        {
+            case '(': return 0;
+            case ')': return 1;
+            case '+': return 2;
+            case '*': return 3;
+            case '/': return 3;
+            case '%': return 4;
+            case '^': return 5;
+            default: return 6;
+        }
+    }
+}
---
+++ Assets/Scripts/ControllerInformation.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ControllerInformation : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _textInformation;
+    private ControllOperator _controllOperators;
+    private char _lastChar;
+    private char _lastOperator;
+    
+    public void AcceptInformation(Text newTextInformation)
+    {
+        Display(CheckInformation(newTextInformation));
+    }
+    public void AllClear()
+    {
+        _textInformation.text = string.Empty;
+        _lastChar = ' ';
+        _lastOperator = ' ';
+    }
+    public void ClearLastChar()
+    {
+        if (_textInformation.text.Length == 0) return;
+        _textInformation.text = _textInformation.text.Remove(_textInformation.text.Length - 1);
+        if (_textInformation.text.Length - 1 >= 0)
+            _lastChar = _textInformation.text[_textInformation.text.Length - 1];
+        SetLastChar();
+    }
+    private void Start()
+    {
+        _controllOperators = new ControllOperator();
+    }
+    private void Display(string text)
+    {
+        _textInformation.text += text;
+        DeleteOtherChar();
+    }
+    private string CheckInformation(Text newTextInformation)
+    {
+        SetLastChar();
+        char charOperation;
+        if (!char.IsDigit(char.Parse(newTextInformation.text)))
+        {
+            if (char.TryParse(newTextInformation.text, out charOperation))
+            {
+                if (charOperation == ',' && _lastOperator == ',') return string.Empty;
+                else if ((charOperation == '(' || (charOperation == '-' && _lastOperator != '-'))) return charOperation.ToString();
+                else if (_textInformation.text.Length != 0)
+                {
+                    if (!char.IsDigit(_lastChar) && !IsArc(_lastChar) && !IsArc(charOperation))
+                    {
+                        _textInformation.text = _textInformation.text.Remove(_textInformation.text.Length - 1);
+                        newTextInformation.text = charOperation.ToString();
+                        return newTextInformation.text;
+                    }
+                    else return newTextInformation.text;
+                }
+            }
+        }
+        else return newTextInformation.text;
+        return string.Empty;
+    }
+    private void SetLastChar()
+    {
+        for (int i = 0; i < _textInformation.text.Length; i++)
+        {
+            _lastChar = _textInformation.text[i];
+            if (_controllOperators.IsChar(_lastChar, "-+/*^%,"))
+                _lastOperator = _lastChar;
+        }
+    }
+    private bool IsArc(char ñ)
+    {
+        if (("()").IndexOf(ñ) != -1)
+            return true;
+        return false;
+    }
+    private void DeleteOtherChar()
+    {
+        for (int i = 0; i < _textInformation.text.Length; i++)
+        {
+            if (_controllOperators.IsChar(_textInformation.text[i], "-,+/*^()%0123456789") == false)
+            {
+                _textInformation.text = _textInformation.text.Remove(i, 1);
+                i--;
+            }
+        }
+    }
+
+}
